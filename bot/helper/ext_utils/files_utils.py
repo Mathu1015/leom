85419c265922088788529be8a536ea349e9c9283
Mathu1015 <unora4281@gmail.com>
2025-06-10 19:50:10 +0530

    Update files_utils.py

--- bot/helper/ext_utils/files_utils.py
+++ bot/helper/ext_utils/files_utils.py
@@ -449,8 +449,6 @@ async def process_file(file_, user_id, dirpath=None, is_mirror=False):
     if metadata_key and dirpath and is_mkv(file_):
         file_ = await change_metadata(file_, dirpath, metadata_key)
  
-remname = user_dict.get("remname", "SUNNXT:SNXT|- Telly|DDH|Tam +:Tamil\s:1|Tam]:Tamil]|Hin]: Hindi]|Tel +:Telugu\s:1|Hin +:Hindi\s:1|Mal +:Malayalam\s:1|Kan +:Kannada\s:1|Kor]:Korean]\s:1|Eng +: English|Jap]: Japanese]|Esubs|Eng]:English]|_White_|- ESub|@World4kMovie - |- Leyon|ENG: English| Esub|-XtRoN|XtRoN|.AMZN.WEB-DL.Multi.Audio.DDP.5.1.: AMZN WEB-DL [Tamil + Telugu + Hindi + Mandarin (DD+2.0 - 224kbps)] |.JIOHS.WEB-DL.Multi.Audio.DDP.5.1: DSNP WEB-DL [Tamil + Telugu + Hindi + English (DD+ 5.1 - 192kbps)] |.H265:×265|.H264:×264|RJTV: RAJTV|_Esub_|.JIOHS.: DSNP |ANToNi|.MX.WEB-DL.Multi.Audio.AAC.2.0: MX WEB-DL [Tamil + Telugu + Hindi (AAC 2.0 - 127kbps)] |.Multi.Audio.AAC.2.0: [Tamil + Telugu + Hindi + English (AAC 2.0 - 128kbps)] |.Tamil.AAC.2.0.: [Tamil (AAC 2.0 - 128kbps)] | .Tamil.DDP.5.1.: Tamil (DD+5.1 - 192kbps)] |")
-
     file_ = re_sub(r"^www\S+\s*[-_]*\s*", "", file_)
     if remname:
         if not remname.startswith("|"):
